c4310f85cdce84453691518c5bdcfc3df1b5b36f
adria <adriacortzss@gmail.com>
2025-02-13 15:52:56 -0300

    Mudanças no cadastro.html, no main.js e na paginadeerro.html

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function goToPage() {
   location.href = "paginadeerro.html";
 }
+function goToPage() {
+  location.href = "cadastro.html";
+}
+
+function goToPage() {
+  location.href = "main.html";
+}
